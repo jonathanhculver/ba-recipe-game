6b15f75a785a305b6ff0fd76a679bcf472a5fcc6
Jon Culver <jonathanhculver@gmail.com>
2015-07-25 15:08:56 -0400

    header and loading of recipes

--- public/js/script.js
+++ public/js/script.js
@@ -8,11 +8,45 @@ ba.game = (function(){
 	var self = {};
 
 	self.init = function() {
-
+		var $loading = $('#loading');
+		ba.game.recipe.init();
 		ba.helper.ajax('/api/menu/', {}, function(data){
-			console.log(data);
+			populateRecipes(data);
+			$loading[0].style.display = 'none';
 		});
-	
+	};
+
+	var populateRecipes = function(data) {
+		var $recipesContainer = $('#recipesContainer');
+		for(var i =0; i<data.length; i++) {
+			ba.game.recipe.buildRecipe(data[i], function($recipeHtml){
+				if(i> 0 && i % 3 == 0) {
+					$recipesContainer.append("<div class='grid_clear'></div>");
+				}
+				$recipesContainer.append($recipeHtml);
+			});
+		}
+		$recipesContainer[0].style.display = 'block';
+	};
+
+	return self;
+})();
+
+ba.game.recipe = (function(){
+	var self = {},
+		template;
+
+	self.init = function() {
+		template = $('#recipes .recipe')[0];
+	};
+
+	self.buildRecipe = function(recipe, cb) {
+		var $recipeHtml = $(template).clone(),
+			img = $recipeHtml.find('img');
+		console.log(img);
+		$recipeHtml[0].style.display = 'block';
+		img.attr('src', recipe.recipe.c_main_dish_image.high_feature);
+		cb($recipeHtml);
 	};
 
 	return self;
