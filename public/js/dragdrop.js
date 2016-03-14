695f9042123ed528cc649e26ca89514fc84aa0ce
Jon Culver <jon.culver@blueapron.com>
2016-03-14 13:30:09 -0400

    ugh why did I have tabs

--- public/js/dragdrop.js
+++ public/js/dragdrop.js
@@ -1,98 +1,98 @@
 /* custom jQuery plugin for drag and drop */
 (function($) {
-	
-	var $dragging = null;
-	
-	$.fn.draggable = function(action) {
-		var startX,
-			startY,
-			offset,
-			offsetX,
-			offsetY,
-			$target = null;
 
-		var attachEvents = function(obj) {
-			$(obj).on('mousedown', function(event){
-				var $emptyCard = $(this).closest('.card_container').find('.card_empty');
-				startX = event.clientX;
-				startY = event.clientY;
-				offset = $(this).position();
-				offsetX = offset.left;
-				offsetY = offset.top;
-				$target = this;
+  var $dragging = null;
 
-				this.style.position = 'absolute';
-				this.style.top = offsetY+"px";
-				this.style.left = offsetX+"px";
-				this.style.zIndex = 1000;
-				$(this).addClass('dragging');
-				$dragging = this;
+  $.fn.draggable = function(action) {
+    var startX,
+      startY,
+      offset,
+      offsetX,
+      offsetY,
+      $target = null;
 
-				$emptyCard.show();
-				
-				// prevents text selection
-				return false;
-			});
-			$(document).on("mousemove", function(event) {
-				// we are dragging something
-				if($target !== null) {
-					$target.style.left = (offsetX + event.clientX - startX) + "px";
-					$target.style.top = (offsetY + event.clientY - startY) + "px";
-				}
-			});
-			$(document).on('mouseup', function(event){
-				// we drop it
-				if($target !== null) {
-					ba.game.scoreboard.scoreMove($target, offsetX, offsetY);
-					$($target).removeClass('dragging');
-					$target = null;	
-					$dragging = null;
-				}
-			});
-		};
+    var attachEvents = function(obj) {
+      $(obj).on('mousedown', function(event){
+        var $emptyCard = $(this).closest('.card_container').find('.card_empty');
+        startX = event.clientX;
+        startY = event.clientY;
+        offset = $(this).position();
+        offsetX = offset.left;
+        offsetY = offset.top;
+        $target = this;
 
-		var reset = function(obj) {
-			var $emptyCard = $(obj).closest('.card_container').find('.card_empty');
-			$emptyCard.hide();
-			obj.style.position = '';
-			obj.style.top = "";
-			obj.style.left = "";
-			$(obj).unbind();
-			$(document).unbind();
-		};
+        this.style.position = 'absolute';
+        this.style.top = offsetY+"px";
+        this.style.left = offsetX+"px";
+        this.style.zIndex = 1000;
+        $(this).addClass('dragging');
+        $dragging = this;
 
-		return this.each(function(){
-			if(action === 'init') {
-				attachEvents(this);
-			}else if(action === 'reset') {
-				reset(this);
-			}
-		});
-	};
+        $emptyCard.show();
 
-	$.fn.droppable = function(action) {
+        // prevents text selection
+        return false;
+      });
+      $(document).on("mousemove", function(event) {
+        // we are dragging something
+        if($target !== null) {
+          $target.style.left = (offsetX + event.clientX - startX) + "px";
+          $target.style.top = (offsetY + event.clientY - startY) + "px";
+        }
+      });
+      $(document).on('mouseup', function(event){
+        // we drop it
+        if($target !== null) {
+          ba.game.scoreboard.scoreMove($target, offsetX, offsetY);
+          $($target).removeClass('dragging');
+          $target = null;
+          $dragging = null;
+        }
+      });
+    };
 
-		var attachEvents = function(obj, image) {
-			$(obj).on('mouseover', function(event){
-				if($dragging !== null) {
-					$(image).addClass('drag_over');
-				}
-			});
+    var reset = function(obj) {
+      var $emptyCard = $(obj).closest('.card_container').find('.card_empty');
+      $emptyCard.hide();
+      obj.style.position = '';
+      obj.style.top = "";
+      obj.style.left = "";
+      $(obj).unbind();
+      $(document).unbind();
+    };
 
-			$(obj).on('mouseout', function(event) {
-				if($dragging !== null) {
-					$(image).removeClass('drag_over');
-				}
-			});
-		};
+    return this.each(function(){
+      if(action === 'init') {
+        attachEvents(this);
+      }else if(action === 'reset') {
+        reset(this);
+      }
+    });
+  };
 
-		return this.each(function(){
-			if(action === 'init') {
-				attachEvents($(this).find('.drop_zone'), $(this).find('.recipeImg'));
-			} else if(action === 'reset') {
-				$(this).find('.drop_zone').unbind();
-			}
-		});
-	};
+  $.fn.droppable = function(action) {
 
-})(jQuery)
+    var attachEvents = function(obj, image) {
+      $(obj).on('mouseover', function(event){
+        if($dragging !== null) {
+          $(image).addClass('drag_over');
+        }
+      });
+
+      $(obj).on('mouseout', function(event) {
+        if($dragging !== null) {
+          $(image).removeClass('drag_over');
+        }
+      });
+    };
+
+    return this.each(function(){
+      if(action === 'init') {
+        attachEvents($(this).find('.drop_zone'), $(this).find('.recipeImg'));
+      } else if(action === 'reset') {
+        $(this).find('.drop_zone').unbind();
+      }
+    });
+  };
+
+})(jQuery);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express"),
-	request = require("request"),
-	ejs = require("ejs");
+  request = require("request"),
+  ejs = require("ejs");
 
 var app = express();
 app.set('views', __dirname + '/views');
@@ -8,21 +8,21 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
-	res.render('index');
+  res.render('index');
 });
 
 app.get('/api/menu/', function(req, res){
-	request("https://ba-js-test.herokuapp.com/api/menu_next_week", function(error, response, body){
-		if(!error && response.statusCode === 200) {
-			var json = JSON.parse(body);
-			res.send(json.two_person_plan.recipes);
-		} else {
-			res.send({"error": error, "status": response.statusCode});
-		}
-	});
+  request("https://ba-js-test.herokuapp.com/api/menu_next_week", function(error, response, body){
+    if(!error && response.statusCode === 200) {
+      var json = JSON.parse(body);
+      res.send(json.two_person_plan.recipes);
+    } else {
+      res.send({"error": error, "status": response.statusCode});
+    }
+  });
 });
 
 
 app.listen(process.env.PORT || 5000, function() {
-	console.log("Listening on port "+ this.address().port);
+  console.log("Listening on port "+ this.address().port);
 });
